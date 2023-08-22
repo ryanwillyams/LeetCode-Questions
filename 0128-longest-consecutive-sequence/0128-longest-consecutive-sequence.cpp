8b2dff2a31ed3d1f337890827e7702928d50554f
Ryan williams <70562805+ryanwillyams@users.noreply.github.com>
2023-08-22 01:23:59 -0700

    Time: 104 ms (81.89%), Space: 45.2 MB (75.06%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int>s(nums.begin(), nums.end());
-        int longest = 0;
-        for(auto &n: s){
-            //if this is the start of the sequence
-            if(!s.count(n - 1)){
-                int length = 1; 
-                while(s.count(n + length))
-                    ++length;
-                longest = max(longest, length);
-            } 
+        if(nums.size() == 0) return 0;
+        std::priority_queue c3(std::less<int>(), nums);
+        int con=1;
+        int maxCon =con;
+        int top;
+        
+        for(int x =c3.size()-1; x >= 0; x--){
+            int toptop = c3.top();
+            c3.pop();
+            top = c3.top();
+             if(top == toptop){
+                 
+                 continue;
+             }
+            
+            if(c3.top() == toptop-1){
+                con++;
+            if(maxCon < con)
+            maxCon = con;
 
-        }
-        return longest;
+            }
+            else{
+                con =1;
+            }
+           
+            }
+       return maxCon;
+
+
+        
     }
 };
