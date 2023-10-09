c1ba2e4b09c9be0f0e5b040b7729d9ec506ef0ba
Ryan williams <70562805+ryanwillyams@users.noreply.github.com>
2023-10-08 21:22:02 -0700

    Time: 30 ms (96.42%), Space: 16.2 MB (88.05%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,18 +1,6 @@
 class Solution:
-    def computeGCD(self, x, y):
-        x = y
-       
-        return abs(x)
-
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        divider = str1 if str1 < str2 else str2
-        divided = str2 if str2 >= str1 else str1
-        maxsub = ""
-        maxiterations = self.computeGCD(len(divided), len(divider))
-        for i in range(maxiterations):
-            sub = divider[:i+1]
-            countDivided = divided.count(sub)
-            countDivider = divider.count(sub)
-            if countDivided*(i+1) == len(divided) and countDivider*(i+1) == len(divider):
-                maxsub = sub
-        return maxsub
+        if str1 + str2 != str2 + str1:
+            return ""
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
